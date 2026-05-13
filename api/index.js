33a4dc3b797a165db795215b842c559c502e57f4
samathaom <writetosamathaom@gmail.com>
2026-05-14 01:06:02 +0530

    chore: ignore node_modules and cleanup repository

--- api/index.js
+++ api/index.js
@@ -1,7 +1,27 @@
-// api/index.js
-const { createProxy } = require('@vencax/netlify-cms-github-oauth-provider');
+const axios = require('axios');
 
-module.exports = createProxy({
-  client_id: process.env.OAUTH_CLIENT_ID,
-  client_secret: process.env.OAUTH_CLIENT_SECRET,
-});
+module.exports = async (req, res) => {
+  const { code, state } = req.query;
+
+  try {
+    const response = await axios.post('https://github.com/login/oauth/access_token', {
+      client_id: process.env.OAUTH_CLIENT_ID,
+      client_secret: process.env.OAUTH_CLIENT_SECRET,
+      code,
+    }, {
+      headers: {
+        Accept: 'application/json',
+      },
+    });
+
+    const { access_token, error } = response.data;
+
+    if (error) {
+      res.status(400).send(`<html><body><script>window.opener.postMessage("authorization:github:error:${error}", "*");</script></body></html>`);
+    } else {
+      res.status(200).send(`<html><body><script>window.opener.postMessage("authorization:github:success:{"token":"${access_token}","provider":"github"}", "*");</script></body></html>`);
+    }
+  } catch (err) {
+    res.status(500).send(err.message);
+  }
+};
