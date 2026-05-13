091858ee005a9f12e56f8a2469e2b276d147c55a
samathaom <writetosamathaom@gmail.com>
2026-05-14 01:47:09 +0530

    cms work - sairam

--- api/auth.js
+++ api/auth.js
@@ -1,7 +1,6 @@
 const axios = require('axios');
 
 module.exports = async (req, res) => {
-  // 1. Grab the temporary code GitHub sent to your browser
   const { code } = req.query;
 
   if (!code) {
@@ -9,7 +8,6 @@ module.exports = async (req, res) => {
   }
 
   try {
-    // 2. Exchange that code for a real Access Token
     const response = await axios({
       method: 'post',
       url: 'https://github.com/login/oauth/access_token',
@@ -19,25 +17,34 @@ module.exports = async (req, res) => {
         code: code,
       },
       headers: {
-        'Content-Type': 'application/json', // Critical!
-        'Accept': 'application/json'        // Tells GitHub to return JSON, not a 404 string
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
       }
     });
 
     const content = response.data;
 
-    // 3. Send the result back to Decap CMS in the format it expects
-    if (content.error) {
-      res.status(400).send(`<html><body><script>
-        window.opener.postMessage("authorization:github:error:${content.error}", "*");
-      </script></body></html>`);
-    } else {
-      res.status(200).send(`<html><body><script>
-        window.opener.postMessage("authorization:github:success:${JSON.stringify(content)}", "*");
-      </script></body></html>`);
-    }
+    // The "Magic" Script: This sends the token back to the CMS window
+    const responseScript = `
+      <script>
+        (function() {
+          function recieve() {
+            window.opener.postMessage(
+              'authorization:github:success:${JSON.stringify({
+                token: content.access_token,
+                provider: 'github'
+              })}',
+              window.location.origin
+            );
+          }
+          recieve();
+        })();
+      </script>
+    `;
+
+    res.status(200).send(responseScript);
+
   } catch (err) {
-    console.error('OAuth Error:', err.response?.data || err.message);
     res.status(500).send(`Auth Failed: ${err.message}`);
   }
 };
