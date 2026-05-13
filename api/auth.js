71b05e657573929aecffd81b4ec8f8237a7f8c7d
samathaom <writetosamathaom@gmail.com>
2026-05-14 01:53:30 +0530

    cms changes sai

--- api/auth.js
+++ api/auth.js
@@ -24,27 +24,37 @@ module.exports = async (req, res) => {
 
     const content = response.data;
 
-    // The "Magic" Script: This sends the token back to the CMS window
+    // Set the header so the browser doesn't just show raw text
+    res.setHeader('Content-Type', 'text/html');
+
+    // This script sends the message and then tries to close itself
     const responseScript = `
-      <script>
-        (function() {
-          function recieve() {
-            window.opener.postMessage(
-              'authorization:github:success:${JSON.stringify({
-                token: content.access_token,
-                provider: 'github'
-              })}',
-              window.location.origin
-            );
-          }
-          recieve();
-        })();
-      </script>
+      <!DOCTYPE html>
+      <html>
+      <body>
+        <script>
+          (function() {
+            const message = "authorization:github:success:${JSON.stringify({
+              token: content.access_token,
+              provider: 'github'
+            })}";
+            
+            // Send the token to the CMS window
+            window.opener.postMessage(message, window.location.origin);
+            
+            // Give it a millisecond to send, then close the popup
+            setTimeout(() => { window.close(); }, 100);
+          })();
+        </script>
+        <p>Authorizing... you can close this window if it doesn't close automatically.</p>
+      </body>
+      </html>
     `;
 
     res.status(200).send(responseScript);
 
   } catch (err) {
+    console.error(err);
     res.status(500).send(`Auth Failed: ${err.message}`);
   }
 };
